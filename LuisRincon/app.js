4d4ccf40f0bd8e4011e3b1bdca705f90f8dd39b5
yudax42 <yudax42@gmail.com>
2021-11-25 00:45:00 +0000

    add animations

--- LuisRincon/app.js
+++ LuisRincon/app.js
@@ -1,16 +1,20 @@
 window.addEventListener("load", function () {
-  let blocks = [
-    { id: "block-1aa355c6d5d04406a8e0e3670b080366", animation: "fade-down" },
-    { id: "block-6f7007a236aa44259c5463e17ce75991", animation: "fade-left" },
-    { id: "block-7c23c29eec4f40678805a16447465b62", animation: "fade-right" },
-    { id: "block-3c4317dc45e24cbba73de68745d7c986", animation: "fade-up" },
-    { id: "block-088cd82d53eb4658bd2af0d121d85e77", animation: "zoom-in" },
-    { id: "block-896cc062886d425ca965583c65292532", animation: "zoom-in" },
-    { id: "block-f75993332e55445ab1c6653ae1995522", animation: "zoom-in" },
+  let elements = Array.from(
+    document.querySelectorAll(".notion-collection-card")
+  );
+  let blocks = elements.map((block) => {
+    return { id: block.getAttribute("id"), animation: "zoom-in" };
+  });
+  blocks = [
+    ...blocks,
+    { id: "block-088cd82d53eb4658bd2af0d121d85e77", animation: "fade-up" },
+    { id: "block-896cc062886d425ca965583c65292532", animation: "fade-up" },
+    { id: "block-e1707b043ab94cca9d0776c1b4fb556b", animation: "fade-up" },
   ];
-
   AOS.init();
+  console.log("dls");
   blocks.forEach((block) => {
+    console.log("block ", block);
     addAnimation(block);
   });
 });
